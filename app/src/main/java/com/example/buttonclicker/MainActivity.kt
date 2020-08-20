f8332967d52201da2f013af98d41995de46d0aa3
Elsa Sheppard <sheppard.elsa03@gmail.com>
2020-08-20 15:10:44 -0700

    Add pop-up messages at 10, 50, and 100 clicks

--- app/src/main/java/com/example/buttonclicker/MainActivity.kt
+++ app/src/main/java/com/example/buttonclicker/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.buttonclicker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 private var timesClicked = 0    // automatically infers that this is an Int     NO MORE PRIMITIVE DATA TYPES
@@ -18,5 +19,16 @@ class MainActivity : AppCompatActivity() {
         timesClicked++
         // display the current number of clicks on the button
         button.setText(timesClicked.toString())
+
+        // give feedback at 10 clicks, 50 clicks, and 100 clicks
+        if (timesClicked == 10)
+            // make a toast to tell the user something about hitting 10
+            Toast.makeText(this, "Achievement unlocked: Double Digits", Toast.LENGTH_LONG).show()
+        if (timesClicked == 50)
+        // make a toast to tell the user something about hitting 10
+            Toast.makeText(this, "Achievement unlocked: Midlife Crisis", Toast.LENGTH_LONG).show()
+        if (timesClicked == 100)
+        // make a toast to tell the user something about hitting 10
+            Toast.makeText(this, "Achievement unlocked: Why are you still clicking?", Toast.LENGTH_LONG).show()
     }
 }
